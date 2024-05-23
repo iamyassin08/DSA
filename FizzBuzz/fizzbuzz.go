c1abf8490d6747572c7b97e633abd823c3bb0fab
iamyassin <yassinmohamed49@gmail.com>
2024-05-23 12:21:03 -0500

    update

--- FizzBuzz/fizzbuzz.go
+++ FizzBuzz/fizzbuzz.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	// Loop from 1 to 100
-	for i := 1; i <= 100; i++ {
+	for i := 0; i <= 100; i++ {
 		// Check if the number is a multiple of both 3 and 5 first,
 		// since it's a special case
 		if i%3 == 0 && i%5 == 0 {
--- LC27-RemoveElement/solution.go
+++ LC27-RemoveElement/solution.go
@@ -1,11 +1,25 @@
+//defines a function named removeElement that takes a slice of integers (nums) and an integer (val) as input parameters and returns an integer.
 func removeElement(nums []int, val int) int {
+
+	//We initialize a pointer left at the start of the array. This pointer will keep track of the position where the next non-val element should be placed.
 	left := 0 // Pointer for non-val elements
+
+	//We use a for loop to iterate through the array using another pointer right.
 	for right := 0; right < len(nums); right++ {
+
+		//Inside the loop, we check if the current element (nums[right]) is not equal to the value val that we want to remove.
 		if nums[right] != val {
+
+			//If the current element is not val, we copy it to the position indicated by the left pointer.
+			// We then move the left pointer one step to the right.
 			nums[left] = nums[right] // Place non-val element at left pointer position
 			left++                   // Move left pointer ahead
 		}
+		//The loop continues until we have checked every element in the array.
 	}
+
+	//After the loop finishes, the left pointer will be at the position one past the last non-val element.
+	// The value of left is the new length of the array with the val elements removed.
 	return left // Number of non-val elements
 }
 
